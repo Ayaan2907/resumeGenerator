2654ed6ae50ac62a020b9a71029681c653e7f4e2
aasimtaif <taifullahansari@gmail.com>
2022-06-12 00:04:28 +0530

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Form from "./components/Form";
-import ResumeList from "./components/List/ResumeList";
 import Body from "./components/Body/Body";
 function App() {
   return (
     <div className="App">
-      
       <Body />
       {/* <ResumeList /> TODO: */}
     </div>
