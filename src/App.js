4d421bfdf3a692d35deeb0d89454a9fb5cbf3022
Ayaan <kaifullah7921@gmail.com>
2022-06-11 23:27:27 +0530

    updated packages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/Form";
 import ResumeList from "./components/List/ResumeList";
+import Body from "./components/Body/Body";
 function App() {
   return (
     <div className="App">
-      <Form />
-      <ResumeList /> {/* TODO: */}
+      
+      <Body />
+      {/* <ResumeList /> TODO: */}
     </div>
   );
 }
